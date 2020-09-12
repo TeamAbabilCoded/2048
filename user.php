a28b446e7eb6596c58a0019c78406c853e4e715b
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2020-09-13 05:49:11 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,1 +1,46 @@
+<?php
 
+$biru="\033[1;34m";
+$turkis="\033[1;36m";
+$ijo="\033[92m";
+$putih="\033[1;37m";
+$pink="\033[1;35m";
+$red="\033[1;31m";
+$kuning="\033[1;33m";
+$t = "\n";
+$r = "\n\n";
+@system("clear");
+
+echo$ijo."menginstall Janda muda anak satu...".$putih."!".$t;
+system('apt install toilet -y');
+sleep(0);
+@system("clear");
+echo$kuning."  ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈".$putih." Indonesia ".$kuning."≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈".$pink."
+  ╔╦╗╔═╗╔═╗╔╦╗  ╔═╗╔╗ ╔═╗╔╗ ╦╦    ╔═╗╔═╗╔╦╗╔═╗╔╦╗".$putih."
+   ║ ║╣ ╠═╣║║║  ╠═╣╠╩╗╠═╣╠╩╗║║    ║  ║ ║ ║║║╣  ║║".$ijo."
+   ╩ ╚═╝╩ ╩╩ ╩  ╩ ╩╚═╝╩ ╩╚═╝╩╩═╝  ╚═╝╚═╝═╩╝╚═╝═╩╝".$t;
+echo$pink." ÷  ".$ijo."Creator".$putih." : ".$kuning."Ṃนhค๓๓ค໓ khคiril".$pink."                       ÷".$t;
+echo$pink." ÷  ".$ijo."YouTube".$putih." : ".$kuning."TEAM ABABIL CODED".$pink."                      ÷".$t;
+echo$pink." ÷  ".$ijo."Support".$putih." : ".$kuning."Cocentz404".$putih." & ".$kuning."Aneuk Cabak".$putih." & ".$kuning."AkasakaId".$pink."   ÷".$t;
+echo$pink." ÷  ".$ijo."Script".$putih."  : ".$kuning."2048 Buddle".$pink."                            ÷".$t;
+echo$pink." ÷  ".$ijo."YouTube".$putih." : ".$kuning."Cocentz404".$putih." & ".$kuning."Aneuk Cabak".$pink."               ÷".$t;
+echo$pink." ÷  ".$ijo."Version".$putih." : ".$kuning."v1.0".$pink."                                   ÷".$t;
+echo$pink." ÷  ".$red."Server".$putih."  : ".$ijo."Online".$pink."                                 ÷".$t;
+echo$pink." ÷ ".$turkis."        Block Reupload Other Than Indonesia".$pink."       ÷".$t;
+echo$kuning." ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈".$putih." Aceh Tamiang ".$kuning."≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈".$r;
+
+echo$putih."[".$red."+".$putih."]".$putih." Indonesia/English".$t;
+echo$putih."[".$red."+".$putih."]".$red." Ini Adalah Program".$putih." Ilegal.!!!".$t;
+echo$putih."[".$red."+".$putih."]".$red." Semua Resiko Tan".$putih."ggung Penguna".$t;
+echo$putih."[".$red."+".$putih."]".$red." This is an ill".$putih."egal program".$t;
+echo$putih."[".$red."+".$putih."]".$red." All risks ar".$putih."e borne by the user".$t;
+echo"$r";
+echo$putih."[".$ijo."√".$putih."]".$biru." Starting Bot 2048 Buddle";
+$ar=0;$is=3;while($ar<$is){
+sleep(1);
+$ar++;
+echo $putih.".";
+}
+sleep(1);
+echo $putih."!".$r;
+sleep(1);
